720e0a5b8d099ecb5491e752b6295f20526da896
Medi Mule <medhanitmulatu6796@gmail.com>
2025-11-08 00:58:08 +0300

    all component and their style are inserted

---
+++ apple-react/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Nav from "./Components/Nav/Nav";
+import MainSection from "./Components/MainSection/MainSection";
+import Footer from "./Components/Footer/Footer";
+
+
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <MainSection />
+      <Footer />
+      
+    </>
+  );
+}
+
+export default App;
